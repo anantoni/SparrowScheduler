5e380f033db7c4ddb42dc1f89ed6d032462c09e4
anantoni <a.antoniadis1989@gmail.com>
2014-08-14 16:15:30 +0300

    unify functionality

---
+++ src/httpscheduler/RequestHandler.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package httpscheduler;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.MethodNotSupportedException;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpRequestHandler;
+import org.apache.http.util.EntityUtils;
+
+/**
+ *
+ * @author thomas
+ */
+class RequestHandler implements HttpRequestHandler  {
+    private final ThreadPoolExecutor taskCommExecutor;         
+
+    // Pass reference to the requestsQueue to the RequestHandler
+    public RequestHandler(ThreadPoolExecutor taskCommExecutor) {
+        super();
+        this.taskCommExecutor = taskCommExecutor;
+    }
+
+    @Override
+    public void handle(
+            final HttpRequest request,
+            final HttpResponse response,
+            final HttpContext context) throws HttpException, IOException {
+
+        String method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
+        if (!method.equals("GET") && !method.equals("HEAD") && !method.equals("POST")) {
+            throw new MethodNotSupportedException(method + " method not supported");
+        }
+
+        if (request instanceof HttpEntityEnclosingRequest) {
+            HttpEntity httpEntity = ((HttpEntityEnclosingRequest) request).getEntity();
+            String entity = EntityUtils.toString(httpEntity);
+            System.out.println("Incoming entity content (string): " + entity);
+
+            // Parse HTTP request
+            Map<String,String> requestArgs = parseHttpSchedulerRequest(entity);
+            StringEntity stringEntity;
+            // If task request
+            if (requestArgs.size() == 3) {
+                assert requestArgs.containsKey("job-id") && requestArgs.containsKey("tasks") && requestArgs.containsKey("task-ids");
+    
+                String jobID = requestArgs.get("job-id");
+                String tasksToSend = requestArgs.get("tasks");
+                String taskIDs = requestArgs.get("task-ids");
+                System.out.println(jobID);
+                System.out.println(tasksToSend);
+                System.out.println(taskIDs);
+                // replace all + with spaces
+                //taskToProcess = taskToProcess.replaceAll("\\+", " ");
+                //Thread taskCommExecutorThread = new TaskCommExecutorThread(taskToProcess);
+                //Future threadMonitor = taskCommExecutor.submit(taskCommExecutorThread);
+                
+//                try {
+//                    // the main thread should wait until the submitted thread
+//                    // finishes its computation
+//                    threadMonitor.get();
+//                } catch (InterruptedException | ExecutionException ex) {
+//                    Logger.getLogger(RequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+                
+                response.setStatusCode(HttpStatus.SC_OK);
+                stringEntity = new StringEntity("result:success");
+            } 
+            else{
+                response.setStatusCode(HttpStatus.SC_OK);
+                stringEntity = new StringEntity("result:fail");
+            }
+               
+            response.setEntity(stringEntity); 
+        }
+    }
+
+    // Parse HTTP requests with the following form: 
+    // job-id=1&task-command=sleep+240s
+    Map<String, String> parseHttpSchedulerRequest(String httpRequest) {
+        String[] requestArguments = httpRequest.split("&");
+        Map<String, String> argsMap = new HashMap<>();
+
+        if (requestArguments.length != 2 && requestArguments.length != 1 ) {
+            System.err.println("Invalid HTTP request: " + httpRequest);
+            return null; 
+        }
+        else if (requestArguments.length == 2) {
+            int counter = 0;
+            for ( String arg : requestArguments ) {
+                String[] keyValuePair = arg.split("=");
+                if (counter == 0) {
+                    if ( keyValuePair[0].equals("job-id") ) {
+                        assert keyValuePair[1].matches("[0-9]+");
+                        argsMap.put( keyValuePair[0], keyValuePair[1]);
+                    }
+                    else {
+                        System.err.println("Invalid argument - expecting job-id");
+                    }
+                }
+                else if (counter == 1) {
+                    if ( keyValuePair[0].equals("task-command") ) {
+                        argsMap.put( keyValuePair[0], keyValuePair[1]);
+                    }
+                    else {
+                        System.err.println("Invalid argument - expecting task-command");
+                    }
+                }
+                counter++;
+            }
+        }
+        else if (requestArguments.length == 1) {
+            String[] keyValuePair = requestArguments[0].split("=");
+            if ( keyValuePair[0].equals("probe") ) 
+                argsMap.put( keyValuePair[0], keyValuePair[1]);
+            else 
+                System.err.println("Invalid argument - expecting probe request");
+        }
+        return argsMap;
+    }
+}
+
